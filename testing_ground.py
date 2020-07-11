b32f1d7f413ebc7979749decb95f87f515e868ab
Vasiliy Shimko <basilshim@gmail.com>
2020-07-11 18:55:24 +0300

    Update testing_ground.py

--- testing_ground.py
+++ testing_ground.py
@@ -3,5 +3,5 @@ import pandas as pd
 from gensim.models import Word2Vec
 
 model = Word2Vec.load("word2vec.model")
-#print(model.most_similar(positive=['ел','говорить'], negative=['есть'], topn=5)) # должно было выдать говорил. В вкабуляре он есть
-print(model.most_similar('говорил'))
+print(model.most_similar(positive=['хорошо','плохой'], negative=['хороший'], topn=5))
+print(model.most_similar('сложный'))
